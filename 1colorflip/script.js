dbcaaa7faf9e2c62c81fc8480a847b5f97f299b6
Seyf <seyanardag@gmail.com>
2022-04-26 17:50:08 +0300

    stop disco button added

--- 1colorflip/script.js
+++ 1colorflip/script.js
@@ -1,19 +1,23 @@
-let bgcolor='red'
-let bodyClass= document.querySelector(".bodyClass")
-let parag = document.getElementsByTagName("p")
+let bgcolor = "red";
+let bodyClass = document.querySelector(".bodyClass");
+let parag = document.getElementsByTagName("p");
 
 function btnClick() {
-    let rnd1=Math.floor(Math.random()*255)
-    let rnd2=Math.floor(Math.random()*255)
-    let rnd3=Math.floor(Math.random()*255)
-    bgcolor = `rgb(${rnd1}, ${rnd2}, ${rnd3})`
-    console.log(bgcolor)  
-    bodyClass.style.backgroundColor=bgcolor
-    parag[0].innerText ="Background Color:"+ bgcolor
+  let rnd1 = Math.floor(Math.random() * 255);
+  let rnd2 = Math.floor(Math.random() * 255);
+  let rnd3 = Math.floor(Math.random() * 255);
+  bgcolor = `rgb(${rnd1}, ${rnd2}, ${rnd3})`;
+  console.log(bgcolor);
+  bodyClass.style.backgroundColor = bgcolor;
+  parag[0].innerText = "Background Color:" + bgcolor;
 }
 
+var discoInterval=null
 function btnDiscoClick() {
-  setInterval(() => {
-    btnClick()
-  }, 50);
+  discoInterval = setInterval(btnClick, 150);
+  
+}
+
+function btnDiscoStopClick() {
+  clearInterval(discoInterval);
 }
