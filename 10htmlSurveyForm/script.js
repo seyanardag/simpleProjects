23e8fef5efc4c60c21b2aa9ba042bc9b4d3222ae
Seyf <seyanardag@gmail.com>
2022-08-24 18:47:07 +0300

    update

---
+++ 10htmlSurveyForm/script.js
@@ -0,0 +1,19 @@
+
+
+
+function changeColorFunc(value) {
+  switch (value) {
+    case "blue":
+        document.body.style.backgroundColor="#93CAED"
+      break;
+    case "yellow":
+        document.body.style.backgroundColor="#F3E0BE"
+      break;
+    case "pink":
+        document.body.style.backgroundColor="#F7CAC9"
+      break;
+
+    default:
+      break;
+  }
+}
